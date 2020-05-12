c612d10e70327e5e71906f4bb6094073ee7cbea4
Meysam2FanLaptop <Meysam2fan@Gmail.com>
2020-05-12 21:31:28 +0430

    newclone

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app', ['activePage' => 'dashboard', 'titlePage' => __('Dashboard')])
 
 @section('content')
+
   <div class="content">
     <div class="container-fluid">
       <div class="row">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container" style="height: auto;">
   <div class="row justify-content-center">
       <div class="col-lg-7 col-md-8">
-          <h1 class="text-white text-center">{{ __('Welcome to Meysam OK unVPN') }}</h1>
+          <h1 class="text-white text-center">{{ __('Welcome to Meysam New Clone') }}</h1>
       </div>
   </div>
 </div>
